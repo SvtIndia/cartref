d95e69ef28e5bfa61e14d4db2038d3839aa3bf18
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2023-09-08 11:08:22 +0530

    empty cart bug fix

--- codes/app/CartStorage.php
+++ codes/app/CartStorage.php
@@ -27,39 +27,37 @@ class CartStorage {
 
     public function put($key, $value)
     {
+        // dd($key);
         $obj = json_decode(json_encode($value));
-        $myKey = array_keys((array)$obj)[0];
-        $myObj = $obj->$myKey;
-        // dd($myObj);
-
+        $myKey = array_keys((array)$obj)[0] ?? 0;
+        $myObj = $obj->$myKey ?? (object)[];
         if($row = Cart::find($key))
-        // if(false)
         {
             // update
             $row->wishlist_data = $value;
 
             $row->cartrowid = $myKey;
-            $row->name = $myObj->name;
-            $row->price = $myObj->price;
+            $row->name = $myObj->name ?? null;
+            $row->price = $myObj->price ?? 0;
             $row->user_id = auth()->user()->id ?? 0;
-            $row->quantity = $myObj->quantity;
-            $row->attributes = json_encode($myObj->attributes);
-            $row->conditions = json_encode($myObj->conditions);
+            $row->quantity = $myObj->quantity ?? 0;
+            $row->attributes = json_encode($myObj->attributes ?? []);
+            $row->conditions = json_encode($myObj->conditions ?? []);
             $row->save();
         }
         else
         {
-            
+
             Cart::create([
                 'id' => $key,
                 'user_id' => auth()->user()->id ?? 0,
                 'wishlist_data' => $value,
                 'cartrowid' => $myKey,
-                'name' => $myObj->name,
-                'price' => $myObj->price,
-                'quantity' => $myObj->quantity,
-                'attributes' => json_encode($myObj->attributes),
-                'conditions' => json_encode($myObj->conditions),
+                'name' => $myObj->name ?? null,
+                'price' => $myObj->price ?? 0,
+                'quantity' => $myObj->quantity ?? 0,
+                'attributes' => json_encode($myObj->attributes ?? []),
+                'conditions' => json_encode($myObj->conditions ?? []),
             ]);
         }
     }
