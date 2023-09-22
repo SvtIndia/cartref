9627efff7375489be4359eaea8eb259b22405853
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2023-09-22 13:11:28 +0530

    Shopping bag link

--- codes/resources/views/livewire/bag/bag_header.blade.php
+++ codes/resources/views/livewire/bag/bag_header.blade.php
@@ -1,6 +1,6 @@
 @if (\Request::route()->getName() != 'ordercomplete')
 <div class="step-by pr-4 pl-4">
-    <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'bag') active @endif"><a>1. Shopping {{ Config::get('icrm.cart.name') }}</a></h3>
+    <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'bag') active @endif"><a href="{{ route('bag') }}">1. Shopping {{ Config::get('icrm.cart.name') }}</a></h3>
     <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'checkout') active @endif"><a>2. Checkout</a></h3>
     <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'ordercomplete') active @endif"><a>3. Order Complete</a></h3>
 </div>
