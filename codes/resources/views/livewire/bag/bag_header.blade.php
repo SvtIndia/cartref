1e106c1e58fbfb682241d7dbf4a192348929ca2e
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2023-09-26 13:02:58 +0530

    label align and green tick

--- codes/resources/views/livewire/bag/bag_header.blade.php
+++ codes/resources/views/livewire/bag/bag_header.blade.php
@@ -1,7 +1,34 @@
+<style>
+    @media screen and (max-width: 480px) {
+        .step-label {
+            width: 100%;
+            text-align: left;
+            padding-left: 7rem !important;
+        }
+
+        .step-by .title.title-step {
+            margin: 0 3.4rem 0 0rem !important;
+        }
+    }
+    .d-icon-check{
+        color: green;
+        margin-left: 1rem;
+    }
+</style>
 @if (\Request::route()->getName() != 'ordercomplete')
-<div class="step-by pr-4 pl-4">
-    <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'bag') active @endif"><a href="{{ route('bag') }}">1. Shopping {{ Config::get('icrm.cart.name') }}</a></h3>
-    <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'checkout') active @endif"><a>2. Checkout</a></h3>
-    <h3 class="title title-simple title-step @if(\Request::route()->getName() == 'ordercomplete') active @endif"><a>3. Order Complete</a></h3>
-</div>
+    <div class="step-by pr-4 pl-4">
+        <h3 class="title title-simple title-step step-label @if (\Request::route()->getName() == 'bag') active @endif">
+            <a href="{{ route('bag') }}">1. Shopping {{ Config::get('icrm.cart.name') }}
+                @if (\Request::route()->getName() == 'checkout')
+                    <i class="d-icon-check"></i>
+                @endif
+            </a>
+        </h3>
+        <h3 class="title title-simple title-step step-label @if (\Request::route()->getName() == 'checkout') active @endif">
+            <a>2. Checkout</a>
+        </h3>
+        <h3 class="title title-simple title-step step-label @if (\Request::route()->getName() == 'ordercomplete') active @endif">
+            <a>3. Order Complete</a>
+        </h3>
+    </div>
 @endif
