145822745e5484067e318467407a2c67967b255d
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2023-11-01 11:54:28 +0530

    Mail logo

--- codes/resources/views/vendor/mail/html/header.blade.php
+++ codes/resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,19 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+            @else
+                <img    
+                    src="{{ Voyager::image(setting('site.logo')) }}" 
+                    style="width: 200px;background: black;padding: 15px;border-radius: 2px;" 
+                    class="logo" 
+                    alt="Cartref Logo"
+                >
+                <br>
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
+    
